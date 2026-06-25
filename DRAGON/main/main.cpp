3738482495cf72ad439dece2e7208e73a81188cf
Bobbie Markwick <rnmarkwick@gmail.com>
2026-06-24 22:33:16 -0400

    Moved main into folder

---
+++ DRAGON/main/main.cpp
@@ -0,0 +1,28 @@
+//  DRAGON
+//  Created by Bobbie Markwick
+//
+
+#include <chrono>
+#include "Problem.hpp"
+
+#ifdef TESTMODE
+#error Test mode was enabled, check your build settings
+#endif
+
+
+int main(int argc, const char * argv[]) {
+    
+    auto start = std::chrono::system_clock::now();
+
+    Advanceable& problem = Problem::makeProblem();
+
+    for(int n = 0; n<10000; n++){
+        if(n>0) problem.advance(0.01);
+        
+        auto now = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = now - start;
+        Problem::cycleComplete(problem, n, elapsed_seconds.count());
+    }
+    
+    return 0;
+}
