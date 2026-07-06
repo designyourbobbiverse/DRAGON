88b3ab21b9c6a3e8b5566873668da32674f18e2a
Bobbie Markwick <rnmarkwick@gmail.com>
2026-07-05 21:20:37 -0400

    Header fixes

--- DRAGON/IO/HDF5_Attrs.hpp
+++ DRAGON/IO/HDF5_Attrs.hpp
@@ -4,48 +4,55 @@
 //
 //  Created by Bobbie Markwick on 03/07/2026.
 //
+#ifndef HDF5_Attrs_hpp
+#define HDF5_Attrs_hpp
+
+#include <string>
 
 //Put all of the attribute names here to ensure consistency
 //Grid elements
-constexpr std::string key_fluid = "/fluid";
-constexpr std::string key_rho = key_fluid + "/rho";
-constexpr std::string key_p = key_fluid + "/p";
-constexpr std::string key_E = key_fluid + "/E";
+inline const std::string key_fluid = "/fluid";
+inline const std::string key_rho = key_fluid + "/rho";
+inline const std::string key_p = key_fluid + "/p";
+inline const std::string key_E = key_fluid + "/E";
 
-constexpr std::string key_v = key_fluid +"/v";
-constexpr std::string key_vx = key_v + "/x";
-constexpr std::string key_vy = key_v + "/y";
-constexpr std::string key_vz = key_v + "/z";
+inline const std::string key_v = key_fluid +"/v";
+inline const std::string key_vx = key_v + "/x";
+inline const std::string key_vy = key_v + "/y";
+inline const std::string key_vz = key_v + "/z";
 
-constexpr std::string key_mom = key_fluid +"/momentum";
-constexpr std::string key_px = key_mom + "/x";
-constexpr std::string key_py = key_mom + "/y";
-constexpr std::string key_pz = key_mom + "/z";
+inline const std::string key_mom = key_fluid +"/momentum";
+inline const std::string key_px = key_mom + "/x";
+inline const std::string key_py = key_mom + "/y";
+inline const std::string key_pz = key_mom + "/z";
 
-constexpr std::string key_B = key_fluid +"/B";
-constexpr std::string key_Bx = key_B + "/x";
-constexpr std::string key_By = key_B + "/y";
-constexpr std::string key_Bz = key_B + "/z";
-constexpr std::string key_A = key_fluid +"/A";
-constexpr std::string key_Ax = key_A + "/x";
-constexpr std::string key_Ay = key_A + "/y";
-constexpr std::string key_Az = key_A + "/z";
+inline const std::string key_B = key_fluid +"/B";
+inline const std::string key_Bx = key_B + "/x";
+inline const std::string key_By = key_B + "/y";
+inline const std::string key_Bz = key_B + "/z";
+inline const std::string key_A = key_fluid +"/A";
+inline const std::string key_Ax = key_A + "/x";
+inline const std::string key_Ay = key_A + "/y";
+inline const std::string key_Az = key_A + "/z";
 
 //Metadata
-constexpr std::string key_fmt = "format_version";
-constexpr std::string key_wrt_opt = "write_option";
-constexpr std::string key_dim = "dim";
-constexpr std::string key_mhd = "MHD";
+inline const std::string key_fmt = "format_version";
+inline const std::string key_wrt_opt = "write_option";
+inline const std::string key_dim = "dim";
+inline const std::string key_mhd = "MHD";
 
-constexpr std::string key_nx = "nx";
-constexpr std::string key_ny = "ny";
-constexpr std::string key_nz = "nz";
-constexpr std::string key_ng = "ng";
-constexpr std::string key_dx = "dx";
-constexpr std::string key_dy = "dy";
-constexpr std::string key_dz = "dz";
-constexpr std::string key_cyc = "cycle";
-constexpr std::string key_time = "time";
+inline const std::string key_nx = "nx";
+inline const std::string key_ny = "ny";
+inline const std::string key_nz = "nz";
+inline const std::string key_ng = "ng";
+inline const std::string key_dx = "dx";
+inline const std::string key_dy = "dy";
+inline const std::string key_dz = "dz";
+inline const std::string key_cyc = "cycle";
+inline const std::string key_time = "time";
 
 //HDF5 File extension
-constexpr std::string file_ext = ".h5";
+inline const std::string file_ext = ".h5";
+
+
+#endif
