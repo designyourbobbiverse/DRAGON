9ff3cb8c5bbea2e787d42559d1c17fe698f0407b
Bobbie Markwick <rnmarkwick@gmail.com>
2026-07-11 00:50:55 -0400

    Updated and Reran Brio&Wu

--- Examples/1D/Brio-Wu/Constants.h
+++ Examples/1D/Brio-Wu/Constants.h
@@ -15,7 +15,6 @@
 constexpr double _gamma = 2.0;
 
 
-
 //******************************************************************//
 //*DO NOT TOUCH anything below this line (but feel free to read it)*//
 //******************************************************************//
@@ -25,7 +24,6 @@ constexpr double _1_4pi = 0.25/M_PI;
 constexpr double _1_8pi = 0.125/M_PI;
 const double sq4pi = sqrt(4.0*M_PI);
 const double sq8pi = sqrt(8.0*M_PI);
-
 
 //MARK: Gamma-dependent constants
 constexpr double  _Gm1_2G = (_gamma - 1.0)/(2.0 * _gamma);
